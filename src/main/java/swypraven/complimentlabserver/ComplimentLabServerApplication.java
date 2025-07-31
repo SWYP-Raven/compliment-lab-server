9ffc4fafd7e930525bb801e0f19c0d603bc7dca7
윾승도 <66948129+tmdeh@users.noreply.github.com>
2025-07-31 23:57:34 +0900

    init

---
+++ src/main/java/swypraven/complimentlabserver/ComplimentLabServerApplication.java
@@ -0,0 +1,13 @@
+package swypraven.complimentlabserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplimentLabServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplimentLabServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/swypraven/complimentlabserver/ComplimentLabServerApplicationTests.java
@@ -0,0 +1,13 @@
+package swypraven.complimentlabserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplimentLabServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
