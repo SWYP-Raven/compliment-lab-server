69b8acf95112dec6ef4aaa408f778a75d065410d
강다연 <142641618+K-Dayeon03@users.noreply.github.com>
2025-08-13 21:23:58 +0900

    커밋 테스트!

--- src/main/java/swypraven/complimentlabserver/ComplimentLabServerApplication.java
+++ src/main/java/swypraven/complimentlabserver/ComplimentLabServerApplication.java
@@ -8,6 +8,7 @@ public class ComplimentLabServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ComplimentLabServerApplication.class, args);
+        System.out.println("커밋");
 	}
 
 }
