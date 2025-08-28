f6eb5078c1f60ac076c4541622bc1904b37c3564
윾승도 <66948129+tmdeh@users.noreply.github.com>
2025-08-28 13:42:15 +0900

    feature: swagger 설정 추가

--- src/main/java/swypraven/complimentlabserver/global/config/SwaggerConfig.java
+++ src/main/java/swypraven/complimentlabserver/global/config/SwaggerConfig.java
@@ -3,22 +3,26 @@ package swypraven.complimentlabserver.global.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
     @Bean
-    public OpenAPI openAPI() {
+    public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .components(new Components())
-                .info(apiInfo());
-    }
-
-    private Info apiInfo() {
-        return new Info()
-                .title("칭찬 연구소")
-                .description("칭찬 연구소REST API")
-                .version("1.0.0");
+                .info(new Info()
+                        .title("칭찬 연구소 API")
+                        .version("v1")
+                        .description("칭찬 연구소 API 명세서"))
+                .addSecurityItem(new SecurityRequirement().addList("JWT"))
+                .components(new Components().addSecuritySchemes("JWT",
+                        new SecurityScheme()
+                                .name("JWT")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
     }
 }
