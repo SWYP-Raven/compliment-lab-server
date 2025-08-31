36e2ce96ad3760fa5b019a62b491cc564fcfb36e
윾승도 <66948129+tmdeh@users.noreply.github.com>
2025-08-31 17:21:25 +0900

    feature: 캐릭터 삭제시 기록

--- src/main/java/swypraven/complimentlabserver/domain/friend/entity/UserFriendType.java
+++ src/main/java/swypraven/complimentlabserver/domain/friend/entity/UserFriendType.java
@@ -4,24 +4,28 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import swypraven.complimentlabserver.domain.compliment.entity.TypeCompliment;
 import swypraven.complimentlabserver.domain.user.entity.User;
 
 @Getter
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "user_friend")
-public class UserFriend {
+@Table(name = "user_friend_type")
+public class UserFriendType {
+
+    public UserFriendType(User user, TypeCompliment typeCompliment) {
+        this.user = user;
+        this.typeCompliment = typeCompliment;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(cascade = CascadeType.ALL)
+    @ManyToOne
     private User user;
 
     @ManyToOne
-    private Friend friend;
-
-
-    private Boolean isDeleted = false;
+    private TypeCompliment typeCompliment;
 }
